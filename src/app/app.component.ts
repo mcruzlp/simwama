8280d7fcec478e95b88163d03b20f3c891a5e31d
mcruzlp <mcruzlp@gmail.com>
2022-02-18 19:40:29 +0100

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { ProductService } from './product.service';
 import { Component } from '@angular/core';
-import { Product } from './product';
-import { Observable } from 'rxjs';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { Product } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +11,6 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class AppComponent {
   products: Observable<Product[]>;
-  formButtonText = 'Add product';
 
   productForm = new FormGroup({
     productId: new FormControl(''),
@@ -22,25 +21,31 @@ export class AppComponent {
     picture: new FormControl(''),
   });
 
+  formButtonText = 'Add product';
+  displayProductForm = false;
+
   constructor(public productService: ProductService) {
     this.products = this.productService.getProducts();
   }
 
   addProduct() {
     this.productService.addProduct(this.productForm.value);
-    this.productForm.reset();
+    this.productForm.reset({ salePrice: 0, purchasePrice: 0, stock: 0 });
+    console.log('addProduct');
   }
 
   updateProductStep1(id: string) {
-    this.productService
-      .getProductById(id)
-      .subscribe((data) => this.productForm.patchValue(data));
+    this.productService.getProduct(id).subscribe((data) => {
+      this.productForm.patchValue(data);
+      console.log('data: ' + JSON.stringify(data));
+    });
 
     this.formButtonText = 'Update product';
   }
 
   updateProductStep2() {
     this.productService.updateProduct(this.productForm.value);
+    console.log('updateProduct');
   }
 
   formSubmit() {
